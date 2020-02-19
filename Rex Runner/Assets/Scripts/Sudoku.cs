2db5eb082d9b6a44b54bc6fa8bb1556659478865
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-19 14:59:16 -0500

    UI Scripts

--- Rex Runner/Assets/Scripts/Sudoku.cs
+++ Rex Runner/Assets/Scripts/Sudoku.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sudoku : MonoBehaviour
 {
+    bool counted = false;
+    public Text Score;
+    public Text HighScore;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-
+        GameObject scoreGO = GameObject.Find("Score");
+        Score = scoreGO.GetComponent<Text>();
+        Score.text = "0";
     }
 
     // Update is called once per frame
@@ -17,6 +23,19 @@ public class Sudoku : MonoBehaviour
         if(gameObject.transform.position.x < -12)
         {
             Destroy(gameObject);
+        }
+        else if(counted == false && gameObject.transform.position.x < -6)
+        {
+            int score = int.Parse(Score.text);
+            score += 1;
+            Score.text = score.ToString();
+            int hScore = int.Parse(HighScore.text);
+            if (score > hScore)
+            {
+                hScore = score;
+                HighScore.text = hScore.ToString();
+            }
+            counted = true;
         }
     }
 }
